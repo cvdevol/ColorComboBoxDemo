b1ff48820cfb7ab6396e1571f74a51a2fb3b13d9
Chris Devol <cvdevol@users.noreply.github.com>
2021-11-03 14:10:13 -0700

    Add files via upload

---
+++ ComboBoxEx.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Windows.Controls.Primitives
+
+''' <summary>
+''' A custom ComboBox that gives direct access to the textbox and popup.
+''' </summary>
+Public Class ComboBoxEx
+    Inherits System.Windows.Controls.ComboBox
+
+    Public Shared TextBoxProperty As DependencyProperty = DependencyProperty.RegisterAttached("TextBox", GetType(TextBox), GetType(ComboBoxEx))
+    Public Property TextBox As TextBox
+        Get
+            Return CType(GetValue(TextBoxProperty), TextBox)
+        End Get
+        Set(value As TextBox)
+            SetValue(TextBoxProperty, value)
+        End Set
+    End Property
+
+    Public Shared PopupProperty As DependencyProperty = DependencyProperty.RegisterAttached("Popup", GetType(Popup), GetType(ComboBoxEx))
+    Public Property Popup As Popup
+        Get
+            Return CType(GetValue(PopupProperty), Popup)
+        End Get
+        Set(value As Popup)
+            SetValue(PopupProperty, value)
+        End Set
+    End Property
+
+
+    Public Sub New()
+        MyBase.New
+
+    End Sub
+
+    Public Overrides Sub OnApplyTemplate()
+        MyBase.OnApplyTemplate()
+
+        TextBox = TryCast(MyBase.GetTemplateChild("PART_EditableTextBox"), TextBox)
+        If TextBox IsNot Nothing Then
+            TextBox.VerticalContentAlignment = VerticalAlignment.Center
+
+        End If
+
+        Popup = TryCast(MyBase.GetTemplateChild("PART_Popup"), Popup)
+        If Popup IsNot Nothing Then
+
+        End If
+    End Sub
+
+
+End Class
---
+++ MainWindow.xaml.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Text
+
+Class MainWindow
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        Dim sb As New StringBuilder
+
+        sb.AppendLine("This is some sample text")
+        sb.AppendLine("to illustrate the use of")
+        sb.AppendLine("the ColorComboBox.")
+        sb.AppendLine()
+        sb.AppendLine("Use MouseWheel while hovering")
+        sb.AppendLine("over the ComboBox")
+        sb.AppendLine("to cycle through colors,")
+        sb.AppendLine("or,")
+        sb.AppendLine("click on the ComboBox")
+        sb.AppendLine("to pick from the DropDown list.")
+
+        TB.Text = sb.ToString
+
+        cbBackground.SelectItem(Brushes.White)
+        cbForeground.SelectItem(Brushes.Black)
+    End Sub
+
+    Private Sub btnWebColors_Click(sender As Object, e As RoutedEventArgs)
+        cbBackground.DisplayType = ColorType.WebColors
+        cbForeground.DisplayType = ColorType.WebColors
+    End Sub
+
+    Private Sub btnSysColors_Click(sender As Object, e As RoutedEventArgs)
+        cbBackground.DisplayType = ColorType.SystemColors
+        cbForeground.DisplayType = ColorType.SystemColors
+    End Sub
+
+    Private Sub btnAllColors_Click(sender As Object, e As RoutedEventArgs)
+        cbBackground.DisplayType = ColorType.AllColors
+        cbForeground.DisplayType = ColorType.AllColors
+    End Sub
+
+    Private Sub cbForeground_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
+        If e.AddedItems.Count = 1 Then
+            Dim ci As ColorItem
+            ci = CType(e.AddedItems(0), ColorItem)
+            TB.Foreground = ci.Brush
+        End If
+    End Sub
+
+    Private Sub cbBackground_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
+        If e.AddedItems.Count = 1 Then
+            Dim ci As ColorItem
+            ci = CType(e.AddedItems(0), ColorItem)
+            TB.Background = ci.Brush
+        End If
+    End Sub
+End Class
